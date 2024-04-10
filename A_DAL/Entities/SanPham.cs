9f947e72237565f89cf5b7b757fae23e04c3e862
TuanHung01 <TuanHung01@DESKTOP-QM5ES3L>
2024-04-10 15:56:22 +0700

    thêm hình ảnh cho sản phẩm

--- A_DAL/Entities/SanPham.cs
+++ A_DAL/Entities/SanPham.cs
@@ -18,6 +18,7 @@ public partial class SanPham
     public int GiaBan { get; set; }
 
     public int TrangThai { get; set; }
+    public byte[]? HinhAnh { get; set; } // Thêm thuộc tính HinhAnh
 
     public virtual ICollection<ChiTietHoaDon> ChiTietHoaDons { get; set; } = new List<ChiTietHoaDon>();
 }
